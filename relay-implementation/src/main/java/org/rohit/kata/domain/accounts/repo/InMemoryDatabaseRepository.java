5d6b4b2fd3ca4066e177e2f7402271adba259da2
Rohit Chavan <53335793+rohitchavan-git@users.noreply.github.com>
2026-07-24 16:49:52 +0530

    Relay: implementation for SCRUM-8

---
+++ relay-implementation/src/main/java/org/rohit/kata/domain/accounts/repo/InMemoryDatabaseRepository.java
@@ -0,0 +1,22 @@
+// Implements: AC-8, AC-10, AC-15
+package org.rohit.kata.domain.accounts.repo;
+
+import org.rohit.kata.domain.accounts.Account;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class InMemoryDatabaseRepository implements AccountRepository {
+    private final List<Account> database = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void save(Account account) {
+        database.add(account);
+    }
+
+    @Override
+    public boolean hasAccount(String accountId) {
+        return database.stream()
+                .anyMatch(account -> account.getAccountId().equals(accountId));
+    }
+}
